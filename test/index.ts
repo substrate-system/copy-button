a8c39b9a0d365d8408fa71143686c6c9303e81b2
Nick Thomas <nichoth@nichoth.com>
2024-03-26 18:18:29 -0700

    try more tests

--- test/index.ts
+++ test/index.ts
@@ -2,9 +2,10 @@ import { test } from '@bicycle-codes/tapzero'
 import Tonic from '@nichoth/tonic'
 import { CopyButton } from '../src/index.js'
 
-test('create the button', async t => {
+test('create the button', t => {
     const tag = Tonic.getTagName(CopyButton.name)
-    document.body.innerHTML = `<${tag}></${tag}>`
+    document.body.innerHTML = `<${tag} class="another-test"></${tag}>`
     const btn = document.querySelector('button')
     t.ok(btn, 'should create a button in the DOM')
+    t.ok(document.querySelector('.another-test'), 'should get element by class')
 })
