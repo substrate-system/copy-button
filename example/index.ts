c6819dee8d8bc0953c1bdbc60a106429517442c3
Nick Thomas <nichoth@nichoth.com>
2024-05-20 22:47:17 -0700

    use native web components

--- example/index.ts
+++ example/index.ts
@@ -1,13 +1,2 @@
-import Tonic from '@bicycle-codes/tonic'
-import { CopyButton, CopySvg, SuccessSvg } from '../src/index.js'
+import '../src/index.js'
 import '../src/style.css'
-
-function CopyExample (this:Tonic) {
-    return this.html`
-        <copy-button class="scroll example" id="example"></copy-button>
-    `
-}
-
-Tonic.add(CopyButton)
-Tonic.add(CopySvg)
-Tonic.add(SuccessSvg)
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,52 @@
-import Tonic from '@bicycle-codes/tonic'
 import clipboardCopy from 'clipboard-copy'
 
-/**
- * __props__
- *   - payload
- */
-export class CopyButton extends Tonic<{ payload }> {
+export class CopyButton extends HTMLElement {
     constructor () {
         super()
-        this.state = { success: false }
-    }
 
-    async click () {
-        clipboardCopy(this.props.payload)
-        this.state.success = true
-        this.reRender()
-        await sleep(2500)
-        this.state.success = false
-        this.reRender()
-    }
+        const payload = this.getAttribute('payload')
+        if (!payload) throw new Error('Missing copy text')
 
-    render () {
+        this.addEventListener('click', async () => {
+            clipboardCopy(payload)
+            // re-render with success check mark
+            this.querySelector('button')!.innerHTML = `${SuccessSvg()}`
+
+            await sleep('2000')
+
+            // re-render with icon
+            this.querySelector('button')!.innerHTML = `${CopySvg()}`
+        })
+
         const classes = ([
             'copy-button',
-            this.props['no-outline'] ? 'no-outline' : '',
-            this.state.success ? 'success' : '',
-            this.props.class
+            this.getAttribute('no-outline') ? 'no-outline' : '',
         ])
             .filter(Boolean)
             .join(' ')
 
-        return this.html`<button
-            class="${classes}"
-        >
-            <span class="copy-svg-wrapper"><copy-svg></copy-svg></span>
-            <span class="success-svg-wrapper"><success-svg><success-svg></span>
+        this.innerHTML =`<button class="${classes}">
+            ${CopySvg()}
         </button>`
     }
 }
 
-export function CopySvg (this:Tonic) {
-    return this.html`<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" class="copy-svg">
-        <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
-    </svg>`
+customElements.define('copy-button', CopyButton)
+
+function CopySvg () {
+    return `<span class="copy-wrapper">
+        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" class="copy-svg">
+            <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
+        </svg>
+    </span>`
 }
 
-export function SuccessSvg (this:Tonic) {
-    return this.html`<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" class="success-svg">
-        <path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>
-    </svg>`
+function SuccessSvg () {
+    return `<span class="success-wraper">
+        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" class="success-svg">
+            <path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>
+        </svg>
+    </span>`
 }
 
 function sleep (ms) {
